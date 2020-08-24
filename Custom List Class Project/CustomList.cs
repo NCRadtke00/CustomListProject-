9829d0af4b2012b989a6b0a9efbc1c6f065897c3
NCRadtke00 <raddnc@gmail.com>
2020-08-24 15:11:23 -0500

    Added methods to CustomList.cs. Created Add method, set count and capacity as var's.

--- Custom List Class Project/CustomList.cs
+++ Custom List Class Project/CustomList.cs
@@ -8,14 +8,50 @@ namespace Custom_List_Class_Project
 {
     public class CustomList<T>
     {
-        public void Add()
+        [] _items;
+
+        int _capacity;
+        int _count;
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+            set
+            {
+                _capacity = value;
+            }
+        }
+
+
+        public CustomList()
+        {
+            _count = 0;
+            _capacity = 4;
+            _items = new T[_capacity];
+        }
+
+        public void Add(T item)
         {
 
+            if (_count == _capacity)
+            {
+                //Something
+            }
+            _items[_count] = item;
+            _count++;
         }
-        public void Remove()
-        {
 
-        }
 
     }
 }
--- Custom List Class Project/Program.cs
+++ Custom List Class Project/Program.cs
@@ -10,6 +10,20 @@ namespace Custom_List_Class_Project
     {
         static void Main(string[] args)
         {
+            //List<T>
+            List<int> myList = new List<int>();
+
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+            myList.Add(5);
+            myList.Add(5);
+            myList.Add(5);
+            myList.Add(5);
+
+            Console.ReadLine();
         }
     }
 }
