2ddbf6d461b01710896cf4100865f3366a30ded7
NCRadtke00 <raddnc@gmail.com>
2020-08-24 14:51:19 -0500

    Added Testing project to solution for CustomListUnitTesting. added AddUnitTesting.cs to testing program

--- Custom List Class Project/CustomList.cs
+++ Custom List Class Project/CustomList.cs
@@ -8,5 +8,14 @@ namespace Custom_List_Class_Project
 {
     public class CustomList<T>
     {
+        public void Add()
+        {
+
+        }
+        public void Remove()
+        {
+
+        }
+
     }
 }
---
+++ CustomListUnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CustomListUnitTesting")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b1a732a7-fc52-47e8-8653-3808cb57b9b9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
